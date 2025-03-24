6a6637ce7f19ccf27ac34042aa9a6dfe45537dc4
lalan-desai-28 <desailalan02@gmail.com>
2025-03-24 11:08:41 +0530

    Tasks of asset images and custom fonts

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_trainee_2025/screens/asset_image_fonts_task_screen.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -9,9 +10,35 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(title: Text("Flutter Training")),
       body: Padding(
         padding: const EdgeInsets.all(3.0),
-        child: Column(children: [
+        child: Column(
+          children: [
+            TaskButton(
+              title: "AssetImage & Fonts",
+              taskScreen: AssetImageFontsTaskScreen(),
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class TaskButton extends StatelessWidget {
+  final String title;
+  final Widget taskScreen;
+  const TaskButton({super.key, required this.title, required this.taskScreen});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: () {
+          Navigator.of(
+            context,
+          ).push(MaterialPageRoute(builder: (context) => taskScreen));
+        },
+        title: Text("AssetImage & Fonts"),
+        trailing: Icon(Icons.arrow_forward),
       ),
     );
   }
